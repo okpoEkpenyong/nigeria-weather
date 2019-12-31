7ed36d72cab837186cc20ded112a367969270d8c
Ekpenyong <okpo.ekpenyong@gmail.com>
2019-12-31 20:39:33 +0100

    fix tests and CI

--- controllers/townsController.js
+++ controllers/townsController.js
@@ -2,12 +2,59 @@ const { pool } = require('../config');
 
 
 const getAllTowns = async (req, res, err) => {
+
+    try {
+        const result = await pool.query(`SELECT * FROM towns ORDER BY tid ASC`);
+       // console.log('result:', result.rows[0].town_name) 
+        if (result.rowCount == 0) { res.send({ message: 'Town data unavailable!' })
+        }
+        else { return res.status(200).send({ status: 'success', data: result.rows, message: 'All towns data retrieved!' });
+        }
+
+    } catch (error) { return res.status(404).send({ status: 'failure', error: error }) }
+}
+
+const getTownNames = async (req, res, err) => {
     try {
         const result = await pool.query(`SELECT town_name FROM towns`);
-        res.status(200).send({ status: 'success', data: result.rows, message: 'data retrieved!' });
-        console.log(`result:`, result.rows)
-    } catch (error) { res.status(404).send({ status: 'failure', error: error}) }
+        if (result.rowCount == 0) { res.send({ message: 'Sorry we could not find that town!' })
+    }
+    else { return res.status(200).send({ status: 'success', data: result.rows, message: 'Town data retrieved!' });
+    }
+
+    } catch (error) { return res.status(404).send({ status: 'failure', error: error }) }
+}
+
+const createTown = async (req, res, err) => {
+    const { town_name, state, region } = req.body;
+
+    try {
+        const result = await pool.query(`INSERT INTO towns(town_name, state, region) VALUES ($1,$2,$3) RETURNING *`, [town_name, state, region]);
+      //  console.log(`result:`, result.rows)
+        return res.status(201).send({ status: 'success', data: result.rows, message: `${town_name} data inserted!` });
+    }
+     catch (error) { res.status(400).send({ status: 'failure', error: error.detail }) }
+}
+
+const createTown2 = (req, res, err) => {
+    const { town_name, state, region } = req.body;
+
+    pool.query(`INSERT INTO towns (town_name, state, region) VALUES ($1,$2,$3) RETURNING *`, [town_name, state, region])
+        .then(res.status(201).send({ status: 'success', data: res.rows, message: `${town_name} data inserted!` }))
+        .catch(res.status(400).send({ status: failure, error: err }))
+}
+
+const deleteTown = async (req, res, err) => {
+    const town_name = req.params.town_name;
+    try {
+        const result = await (pool.query(`DELETE FROM towns WHERE town_name = $1`, [town_name]));
+        if (result.rowCount == 0) {res.send({ data: result.rows, message: `${town_name} does not exist. Maybe its already deleted?`})}
+        else{ return res.status(200).send({ status: 'success', message: `${town_name} town successfully deleted!` })} 
+    }
+    catch (error) { res.status(400).send({ status: 'failure', error: error }) }
+
 }
 
 
-module.exports = { getAllTowns }
+
+module.exports = { getAllTowns, getTownNames, createTown, deleteTown }  
--- routes/townsRoutes.js
+++ routes/townsRoutes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const database = require('../controllers/townsController');
 
 
-router.get('/api/v1/towns', database.getAllTowns);
+router.get('/api/v1/auth/town/', database.getTownNames);
+router.post('/api/v1/auth/create-town/', database.createTown);
+router.delete('/api/v1/auth/towns/:town_name/', database.deleteTown);
+router.get('/api/v1/auth/towns/', database.getAllTowns);
 
 
 
