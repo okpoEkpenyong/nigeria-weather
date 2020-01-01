89dd55e0330d55128ab4f391062aee16c3fb6bcb
Ekpenyong <okpo.ekpenyong@gmail.com>
2020-01-01 08:30:21 +0100

    restructure project

--- test/test.js
+++ test/test.js
@@ -7,17 +7,6 @@ const request = require('supertest');
 const { pool } = require("../config");
 
 
-//const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
-
-const config = {
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
-};
-
-console.log("config : "+JSON.stringify(config));
 
 chai.use(chaiHttp);
 const expect = chai.expect;
@@ -33,111 +22,5 @@ describe('Array', () => {
   });
 });
 
-before(async () => {
-  await pool.query(`CREATE TABLE towns ( 
-    tID SERIAL PRIMARY KEY ,
-    town_name VARCHAR(255) NOT NULL ,
-    state VARCHAR (255)NOT NULL ,
-    region VARCHAR (255) NOT NULL
-  )`);
-});
 
-beforeEach( async () => {
-  await pool.query("INSERT INTO towns (town_name,state,region) VALUES ('Gboko','Benue','North Central')");
-});
-
-afterEach(async () => {
-  await pool.query("DELETE FROM towns");
-});
-
-after(async () => {
-  await pool.query("DROP TABLE towns");
-  pool.end();
-});
-describe("GET /api/v1/auth/towns", () => {
- it(" responds with an array of towns data", async () => {
-    const Towns = await request(app)
-    .get("/api/v1/auth/towns");
-   // console.log('towns:', Towns.body.data[0].tid )
-    expect(Towns.body.data[0]).to.have.property("tid");
-    expect(Towns.body.data[0]).to.have.property("state");
-    expect(Towns.body.data[0]).to.have.property("region");
-    expect(Towns).to.have.status(200);
-    expect(Towns.body.data[0]).to.include({
-    tid:  Towns.body.data[0].tid,
-    town_name: 'Gboko' ,
-    state: 'Benue',
-    region: 'North Central'
-   })
-  });
-
-  it("should not fetch validTown with incorrect url", async () => {
-    const invalidTown = await request(app)
-    .get('/api/v1/auth/toe')
-    expect(invalidTown).to.have.status(404);
-  })
-});
-
-describe("GET /api/v1/auth/town", () => {
-  it(" responds with an array of town names only", async () => {
-     const Towns = await request(app)
-     .get("/api/v1/auth/town");
-     //console.log('towns:', Towns.body.data[0] )
-     expect(Towns.body.data[0]).to.have.property("town_name");
-     expect(Towns).to.have.status(200);
-     expect(Towns.body.data[0]).to.include({
-     town_name: 'Gboko' ,
-    })
-   });
- 
-   it("should not fetch validTown with incorrect url", async () => {
-     const invalidTown = await request(app)
-     .get('/api/v1/auth/toe')
-     expect(invalidTown).to.have.status(404);
-   })
- });
-
-describe("POST /api/v1/auth/create-town/", () => {
-  it("It responds with the newly created town", async () => {
-    const newTown = await request(app)
-      .post("/api/v1/auth/create-town")
-      .send({
-        town_name: 'Aba' ,
-        state: 'Abia',
-        region: 'South East'
-      });
-      expect(newTown.body.data[0]).to.include({
-        tid:  newTown.body.data[0].tid,
-        town_name: 'Aba' ,
-        state: 'Abia',
-        region: 'South East'
-       })
-    expect(newTown).to.have.status(201);
-
-    const response = await request(app)
-    .get("/api/v1/auth/towns");
-    expect(response.body.data.length).to.equal(2);
-  });
-});
-
-describe("DELETE /api/v1/auth/towns/:town_name", () => {
-  it("It responds with a message of Deleted", async () => {
-    const newTown = await request(app)
-      .post("/api/v1/auth/create-town/")
-      .send({
-        town_name: 'Agbor' ,
-        state: 'Delta',
-        region: 'South South'
-      });
-    const removedTown = await request(app)
-    .delete(`/api/v1/auth/towns/${newTown.body.data[0].town_name}`);
-    //console.log('removed:',removedTown )
-    expect(removedTown.body).to.include({ message: `${newTown.body.data[0].town_name} town successfully deleted!` });
-    expect(removedTown).to.have.status(200);
-
-    const response = await request(app)
-    .get("/api/v1/auth/towns");
-    expect(response.body.data.length).to.equal(1);
-  });
-});
 
